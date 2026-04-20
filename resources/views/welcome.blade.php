d90e96dbc99fe9c98be2938c4b275c81e0687ad5
AbelChrisnaldi <abelchris123@gmail.com>
2026-04-20 12:29:59 +0700

    commit ketiga

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Telkom University — Dashboard</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('images/iconiebi.png') }}">
+
     <style>
         * {
             margin: 0;
